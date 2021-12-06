9ff3bfc6620e500069b516c8952d5361de5867a4
Vedat Kaya <vedataydinkayaa@gmail.com>
2021-12-06 23:42:33 +0300

    ViewTable-added

---
+++ Gun 3/ViewTable.sql
@@ -0,0 +1,13 @@
+SELECT *
+FROM [Category Sales for 1997]
+
+-- URUN BAZLI SIPARIS RAPORU
+ALTER VIEW VW_URUNSIPARIS
+AS
+SELECT P.ProductName,CAST(ROUND(SUM((1-OD.Discount)*OD.UnitPrice*OD.Quantity),2) AS decimal(15,3)) TOTAL
+FROM [Order Details] OD
+JOIN Products P ON P.ProductID = OD.ProductID
+GROUP BY P.ProductName
+
+SELECT * FROM VW_URUNSIPARIS
+ORDER BY TOTAL DESC
